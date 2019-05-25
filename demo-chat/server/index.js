f58c5c8180da073318645266dcffb3a2239c771e
Martin Llaneza <martinllaneza@gmail.com>
2019-05-25 23:26:56 +0800

    feat(demo-chat): Add chat demo using lit-element and web socket

---
+++ demo-chat/server/index.js
@@ -0,0 +1,15 @@
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(data) {
+    // Broadcast to everyone else
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    });
+  });
+});
+
+console.log('Websocket server started');
